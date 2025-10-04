93a9e86a888f338ff73b50c5569697556564dff2
Audun Skorstad <audun.skorstadskatteetaten.no>
2025-10-04 14:38:55 +0200

    Updated fix for counter

---
+++ image_metadata_proxy.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: application/json');
+header('Cache-Control: no-cache');
+header('Access-Control-Allow-Origin: *');
+
+$imageUrl = 'https://ramsoy.skorstad.name/siste.jpg';
+
+// Get headers from the image
+$headers = get_headers($imageUrl, 1);
+
+if ($headers && isset($headers['Last-Modified'])) {
+    echo json_encode([
+        'lastModified' => $headers['Last-Modified'],
+        'success' => true
+    ]);
+} else {
+    http_response_code(500);
+    echo json_encode([
+        'error' => 'Could not fetch image headers',
+        'success' => false
+    ]);
+}
+?>
